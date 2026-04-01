864a56f57162072f12db09ce1bd0e90e789585dd
Anupama-Sharma18 <anusharma0418@gmail.com>
2026-04-01 10:17:32 +0530

    fixed getter issue

--- src/main/java/com/chatbot/whatsapp_bot/model/MessageRequest.java
+++ src/main/java/com/chatbot/whatsapp_bot/model/MessageRequest.java
@@ -6,4 +6,11 @@ import lombok.Data;
 public class MessageRequest {
     private String message;
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
